56d0bc579a777378d33dabb080de64ebeae9361b
Vince Martin <vince@finalatomicbuster.net>
2024-06-22 16:00:20 -0700

    Returning JSON finally!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,39 @@
-// Explain the line of code below:
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+use rocket::serde::json::Json;
+use rocket::serde::Serialize;
+
+use std::fmt;
+
+// Use Serialize so that serde/rocket can serialize my struct into JSON.
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+// Ride struct.
+struct Ride {
+    title: String,
+    description: String,
+}
+
+// This is supposed to be my to string...
+impl fmt::Display for Ride {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // What the heck is a Result!?
+        write!(
+            f,
+            "title: {}, description: {}",
+            self.title, self.description
+        )
+    }
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Json<Ride> {
+    let ride = Ride {
+        title: String::from("First ride ever!"),
+        description: String::from("Whoa, so great!  I can't believe it!"),
+    };
+    Json(ride)
 }
 
 #[launch]
