a6b29b17b7c20d5ece0ab79fb7b063de19dab5b2
Vincent Martin <vince@finalatomicbuster.net>
2024-06-23 19:09:16 -0700

    Implement Basic API (#1)
    * Add /ride and /ride/<id> endpoints.
    
    * Wrap up GET and POST

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,78 @@
 #[macro_use]
 extern crate rocket;
 
+use std::vec;
+
 use rocket::serde::json::Json;
-use rocket::serde::Serialize;
+use rocket::serde::{Deserialize, Serialize};
 
-use std::fmt;
+// Note on serialization on the below structs.
+// We use Serialize so that serde/rocket can serialize my struct into JSON.
 
-// Use Serialize so that serde/rocket can serialize my struct into JSON.
-#[derive(Serialize)]
-#[serde(crate = "rocket::serde")]
 // Ride struct.
+#[derive(Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
 struct Ride {
+    id: usize,
     title: String,
     description: String,
 }
 
-// This is supposed to be my to string...
-impl fmt::Display for Ride {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        // What the heck is a Result!?
-        write!(
-            f,
-            "title: {}, description: {}",
-            self.title, self.description
-        )
-    }
+// Rides struct.
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Rides {
+    rides: Vec<Ride>,
 }
 
-#[get("/")]
-fn index() -> Json<Ride> {
+// Return a list of all rides.
+#[get("/ride")]
+fn get_rides() -> Json<Rides> {
+    let rides = Rides {
+        rides: vec![
+            Ride {
+                id: 1,
+                title: String::from("First ride ever!"),
+                description: String::from("Whoa, so great! I can't believe it!"),
+            },
+            Ride {
+                id: 2,
+                title: String::from("Second ride ever!"),
+                description: String::from("Whoa, so great! I can't believe it!"),
+            },
+            Ride {
+                id: 3,
+                title: String::from("Third ride ever!"),
+                description: String::from("Whoa, so great! I can't believe it!"),
+            },
+            Ride {
+                id: 4,
+                title: String::from("Fourth ride ever!"),
+                description: String::from("Whoa, so great! I can't believe it!"),
+            },
+        ],
+    };
+    Json(rides)
+}
+
+// Return a particular ride based on id.
+#[get("/ride/<id>")]
+fn get_ride(id: usize) -> Json<Ride> {
     let ride = Ride {
+        id,
         title: String::from("First ride ever!"),
         description: String::from("Whoa, so great!  I can't believe it!"),
     };
     Json(ride)
 }
 
+// Create a new ride.
+#[post("/ride", data = "<ride>")]
+fn post_ride(ride: Json<Ride>) -> Json<String> {
+    Json(format!("New ride: {}", ride.title))
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![get_ride, get_rides, post_ride])
 }
