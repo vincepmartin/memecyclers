7aa69b7538cb21fcf38e7586cd24a4278c3d83ec
Vincent Martin <vince@finalatomicbuster.net>
2024-12-24 10:31:20 -0800

    Fixed form test no data.

--- server/src/tests.rs
+++ server/src/tests.rs
@@ -1,5 +1,7 @@
-use crate::models::{ApiResponse, InsertableRide, Ride, RideData};
+use crate::models::{ApiResponse, InsertableRide, Ride};
 use crate::rocket;
+use std::fs;
+use std::io::Write;
 
 use rocket::http::{ContentType, Status};
 use rocket::local::blocking::Client;
@@ -30,57 +32,108 @@ fn test_everything() {
     verify_deleted_ride(client);
 }
 
-// Helper functions used in testing.
-fn verify_deleted_ride(client: Client) {
-    let response = client
-        .get("/api/ride/{get_returned_ride.id}")
-        .header(ContentType::JSON)
-        .dispatch();
+#[test]
+fn test_multipart_without_files() {
+    let mut form_data: Vec<u8> = Vec::new();
+    let boundary = "GADGET";
 
-    assert_eq!(
-        response.status(),
-        Status::BadRequest,
-        "Deleted item still exists."
-    );
+    // start
+    write!(form_data, "--{}\r\n", boundary).unwrap();
+
+    // Title
+    write!(
+        form_data,
+        "Content-Disposition: form-data; name=\"title\"\r\n"
+    )
+    .unwrap();
+    write!(form_data, "\r\n").unwrap();
+    write!(form_data, "Test form no data").unwrap();
+    write!(form_data, "\r\n").unwrap();
+
+    // Description
+    write!(form_data, "--{}\r\n", boundary).unwrap();
+    write!(
+        form_data,
+        "Content-Disposition: form-data; name=\"description\"\r\n"
+    )
+    .unwrap();
+    write!(form_data, "\r\n").unwrap();
+    write!(form_data, "Description test information.").unwrap();
+    write!(form_data, "\r\n").unwrap();
+
+    write!(form_data, "--{}--\r\n", boundary).unwrap();
+
+    let client = Client::tracked(rocket()).expect("valid rocket instance");
+    let request = client
+        .post("/api/ride_data/")
+        .header(ContentType::new("multipart", "form-data; boundary=GADGET"))
+        .body(&form_data);
+
+    let response = request.dispatch();
+    assert_eq!(response.status(), Status::Ok);
 }
 
-fn delete_ride(client: &Client, get_returned_ride: ApiResponse<Ride>) {
-    let response = client
-        .delete(format!("/api/ride/{}", get_returned_ride.data.id))
-        .header(ContentType::JSON)
-        .dispatch();
+// #[test]
+// fn test_multipart_form_with_files() {
+//     // **************************
+//     // Test our multipart form.
+//     // **************************
+//     // let ride_data_example = RideData {
+//     //     title: "ride_data_test".to_string(),
+//     //     description: "ride_data_description".to_string(),
+//     //     data: None, // We are not using data here...
+//     // };
+//
+//     // // We are instead using it here...
+//     // // Load our test file.
+//     // let test_file_path = "./storage/test_file.jpg";
+//     // let image_data = fs::read(test_file_path).expect("Failed to load test file.");
+//
+//     // // Craft a string that represents a Form with our test ride.
+//     // // This feels a little gross, but hopefully it works.
+//     // let form_boundary = "--boundary_rando_123";
+//     // let form_data = format!(
+//     //     "{}\nContent-Disposition: form-data; name=\"title\"\n\n{}\n{}\
+//     //     \nContent-Disposition: form-data; name=\"description\"\n\n{}\n{}\
+//     //     \nContent-Disposition: form-data; name=\"data\"; filename=\"test_file.jpg\"\n\
+//     //     Content-Type: image/jpeg\n\n{}\n{}",
+//     //     form_boundary,
+//     //     "ride_data_test", // title
+//     //     form_boundary,
+//     //     "ride_data_description", // description
+//     //     form_boundary,
+//     //     String::from_utf8_lossy(&image_data), // fake file data
+//     //     form_boundary
+//     // );
+//     // println!("*** Here is our stupid form that we are going to send...");
+//     // println!("{}", form_data);
+//     // println!("***");
+//
+//     let test_file_path = "./test/request_two_files.txt";
+//     let form_data = fs::read(test_file_path).expect("Failed to load test request file.");
+//
+//     let client = Client::tracked(rocket()).expect("valid rocket instance");
+//     let request = client
+//         .post("/api/ride_data/")
+//         // TODO Consider changing me.
+//         .header(ContentType::new(
+//             "multipart",
+//             "form-data; boundary=boundary_rando_123",
+//         ))
+//         .body(&form_data);
+//
+//     println!("Our request!!!");
+//
+//     let response = request.dispatch();
+//     println!(
+//         "********************\n
+//         ***** RESPONSE *****\n{}",
+//         String::from_utf8(form_data).expect("Invalid UTF-8 in test data file.")
+//     );
+//     assert_eq!(response.status(), Status::Ok);
+// }
 
-    assert_eq!(response.status(), Status::Ok, "Delete failed.");
-}
-
-fn get_added_ride(
-    response_from_input_ride: ApiResponse<Ride>,
-    client: &Client,
-) -> ApiResponse<Ride> {
-    println!(
-        "**** GETTING RIDE WITH ID {} ****",
-        response_from_input_ride.data.id
-    );
-    let response = client
-        .get(format!("/api/ride/{}", response_from_input_ride.data.id))
-        .header(ContentType::JSON)
-        .dispatch();
-
-    let get_returned_ride_string = response.into_string().unwrap();
-
-    println!("*** Returned ride string... {}", get_returned_ride_string);
-
-    let get_returned_ride: ApiResponse<Ride> =
-        rocket::serde::json::from_str(&get_returned_ride_string)
-            .expect("Failed to deserialize response into a Ride struct.");
-
-    assert_eq!(
-        get_returned_ride.data, response_from_input_ride.data,
-        "POST Ride and GET Ride are not equal!"
-    );
-    get_returned_ride
-}
-
+// *** Helper functions used in testing. ***
 fn add_ride() -> (Client, ApiResponse<Ride>) {
     let input_ride = InsertableRide {
         title: "test_ride_title".to_string(),
@@ -111,24 +164,52 @@ fn add_ride() -> (Client, ApiResponse<Ride>) {
     (client, response_from_input_ride)
 }
 
-// #[test]
-// fn test_multipart_form() {
-//     // ********************
-//     // 1. Test our multipart form.
-//     // ********************
-//     let ride_data_example = RideData {
-//         title: "ride_data_test".to_string(),
-//         description: "ride_data_description".to_string(),
-//         data: //TODO: Figure out how to get binary file here.
-//     };
-//
-//     let client = Client::tracked(rocket()).expect("valid rocket instance");
-//     let response = client
-//         .post("/api/ride/")
-//         .header(ContentType::Form)
-//         // TODO: The next bit is obviously not correct...
-//         .body(rocket::serde::json::to_string(&ride_data_example).unwrap())
-//         .dispatch();
-//
-//     assert_eq!(response.status(), Status::Ok);
-// }
+fn get_added_ride(
+    response_from_input_ride: ApiResponse<Ride>,
+    client: &Client,
+) -> ApiResponse<Ride> {
+    println!(
+        "**** GETTING RIDE WITH ID {} ****",
+        response_from_input_ride.data.id
+    );
+    let response = client
+        .get(format!("/api/ride/{}", response_from_input_ride.data.id))
+        .header(ContentType::JSON)
+        .dispatch();
+
+    let get_returned_ride_string = response.into_string().unwrap();
+
+    println!("*** Returned ride string... {}", get_returned_ride_string);
+
+    let get_returned_ride: ApiResponse<Ride> =
+        rocket::serde::json::from_str(&get_returned_ride_string)
+            .expect("Failed to deserialize response into a Ride struct.");
+
+    assert_eq!(
+        get_returned_ride.data, response_from_input_ride.data,
+        "POST Ride and GET Ride are not equal!"
+    );
+    get_returned_ride
+}
+
+fn delete_ride(client: &Client, get_returned_ride: ApiResponse<Ride>) {
+    let response = client
+        .delete(format!("/api/ride/{}", get_returned_ride.data.id))
+        .header(ContentType::JSON)
+        .dispatch();
+
+    assert_eq!(response.status(), Status::Ok, "Delete failed.");
+}
+
+fn verify_deleted_ride(client: Client) {
+    let response = client
+        .get("/api/ride/{get_returned_ride.id}")
+        .header(ContentType::JSON)
+        .dispatch();
+
+    assert_eq!(
+        response.status(),
+        Status::BadRequest,
+        "Deleted item still exists."
+    );
+}
